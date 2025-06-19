4a074638745becaef39283dd611da8ced91231c7
Tigran Parsadanyan <tigran.parsadanyan@inconceptlabs.com>
2025-06-19 13:27:51 +0400

    Add Retrofit dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,6 +49,10 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.retrofit)
+    implementation(libs.moshi)
+    implementation(libs.loggingInterceptor)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
